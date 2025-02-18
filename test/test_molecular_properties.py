34a2dcda04ba825fc74f3cbd83e0a8e0b0bc87d8
unknown <philippe.form@gmail.com>
2025-02-18 12:10:36 -0500

    [test] Add fixtures

--- test/test_molecular_properties.py
+++ test/test_molecular_properties.py
@@ -21,8 +21,8 @@ def is_rdkit_use(name:str):
 @pytest.fixture(
     params=[
         "hERG*Tox",
-        pytest.param("BBB_Martins*ADME", marks=pytest.mark.slow),
-        pytest.param("Caco2_Wang*ADME", marks=pytest.mark.slow)
+        pytest.param("BBB_Martins*ADME", marks=pytest.mark.skip),
+        pytest.param("Caco2_Wang*ADME", marks=pytest.mark.skip)
     ],
     scope="module"
 )
