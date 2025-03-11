32b3a0d727f89f483539948a3b7ad277b7c1215c
unknown <philippe.form@gmail.com>
2025-03-11 16:08:35 -0400

    Add LM head and Embed Tokens

--- mol_gen_docking/sft_data.py
+++ mol_gen_docking/sft_data.py
@@ -122,8 +122,9 @@ class InstructionDatasetProcessor:
             for col in ["input", "instruction", "output"]
             if col in self.dataset.column_names
         ]
+        print(self.dataset)
         self.dataset = self.dataset.map(
-            self.process_str, num_proc=self.n_proc, remove_columns=cols_to_remove
+            self.process_str, num_proc=self.n_proc, remove_columns=cols_to_remove,
         )
         # If train and test are not specified, flatten the dataset and split it
         if not ("train" in self.dataset.keys() and "test" in self.dataset.keys()):
