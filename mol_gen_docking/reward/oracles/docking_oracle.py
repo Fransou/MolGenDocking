0c9d479eab03c9f1bd3865835de770ebf6749baa
Fransou <philippe.form@gmail.com>
2025-04-11 17:18:22 -0400

    [fix]

--- mol_gen_docking/reward/oracles/docking_oracle.py
+++ mol_gen_docking/reward/oracles/docking_oracle.py
@@ -57,13 +57,11 @@ class PyscreenerOracle:
         final_score = self.scorer(test_smiles)
         if isinstance(test_smiles, str):
             return list(final_score)[0]
-        else:  ## list
-            # dict: {'O=C(/C=C/c1ccc([N+](=O)[O-])o1)c1ccc(-c2ccccc2)cc1': -9.9, 'CCOc1cc(/C=C/C(=O)C(=Cc2ccc(O)c(OC)c2)C(=O)/C=C/c2ccc(O)c(OCC)c2)ccc1O': -9.1}
-            # return [list(i.values())[0] for i in final_score]
+        else:
             score_lst = []
             print(final_score)
-            for smiles in test_smiles:
-                score = final_score[smiles]
+            for i, smiles in enumerate(test_smiles):
+                score = final_score[i]
                 if score is None:
                     score = error_value
                 score_lst.append(score)
