b6de072906711230ebe2be5a25d378ec42b076c9
Fransou <philippe.form@gmail.com>
2025-04-14 09:54:50 -0400

    [fix]

--- mol_gen_docking/reward/oracles/__init__.py
+++ mol_gen_docking/reward/oracles/__init__.py
@@ -1,7 +1,5 @@
 from tdc.oracles import Oracle, oracle_names
 
-from .rdkit_oracle import RDKITOracle
-from .docking_oracle import PyscreenerOracle
 from .utils import (
     propeties_csv,
     PROPERTIES_NAMES_SIMPLE,
@@ -18,10 +16,14 @@ def get_oracle(oracle_name: str):
     oracle_wrapper = OracleWrapper()
     oracle_name = PROPERTIES_NAMES_SIMPLE.get(oracle_name, oracle_name)
     if oracle_name.endswith("docking_vina") or oracle_name.endswith("docking"):
+        from .docking_oracle import PyscreenerOracle
+
         oracle_wrapper.assign_evaluator(PyscreenerOracle(oracle_name, ncpus=16))
     elif oracle_name.lower() in oracle_names:
         oracle_wrapper.assign_evaluator(Oracle(name=oracle_name, ncpus=4), oracle_name)
     else:
+        from .rdkit_oracle import RDKITOracle
+
         oracle_wrapper.assign_evaluator(RDKITOracle(oracle_name), oracle_name)
     return oracle_wrapper
 
