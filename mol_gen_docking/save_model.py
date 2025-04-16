60f4a9e95f5ec902205bd97378db5c8d54207c0e
Fransou <philippe.form@gmail.com>
2025-04-16 13:53:57 -0400

    [model] Script can save models without changing the vocab

--- mol_gen_docking/save_model.py
+++ mol_gen_docking/save_model.py
@@ -8,19 +8,18 @@ import tokenizers
 from mol_gen_docking.data import special_tok
 
 
-def resize_model(model_path: str, output_path: str):
+def resize_model(model_path: str, output_path: str, add_spe_tokens: bool = False):
     model = AutoModelForCausalLM.from_pretrained(model_path)
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-    special_tokens = [
-        tokenizers.AddedToken(t, special=True, lstrip=True, rstrip=True)
-        for _, t in special_tok.items()
-    ]
-    tokenizer.add_special_tokens({"additional_special_tokens": special_tokens})
-    model.resize_token_embeddings(len(tokenizer))
-    model.tie_weights()
-    model.save_pretrained(output_path)
-    tokenizer.save_pretrained(output_path)
+    if add_spe_tokens:
+        special_tokens = [
+            tokenizers.AddedToken(t, special=True, lstrip=True, rstrip=True)
+            for _, t in special_tok.items()
+        ]
+        tokenizer.add_special_tokens({"additional_special_tokens": special_tokens})
+        model.resize_token_embeddings(len(tokenizer))
+        model.tie_weights()
+    return model, tokenizer
 
 
 if __name__ == "__main__":
@@ -39,7 +38,19 @@ if __name__ == "__main__":
         help="The path to save the new model",
         default="qwen_2_0.5B_resized",
     )
+
+    parser.add_argument(
+        "--add-spe-tokens",
+        action="store_true",
+        help="Add special tokens to the model",
+    )
+
     args = parser.parse_args()
 
     os.makedirs(args.output_path, exist_ok=True)
-    resize_model(args.model_path, args.output_path)
+    model, tokenizer = resize_model(
+        args.model_path, args.output_path, args.add_spe_tokens
+    )
+
+    model.save_pretrained(args.output_path)
+    tokenizer.save_pretrained(args.output_path)
