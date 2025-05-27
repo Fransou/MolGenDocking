941ea43cbc910c2d43db49fe2c3b28a96b9e027c
Fransou <philippe.form@gmail.com>
2025-05-27 16:56:29 -0400

    [reward] Throughs a warning if there is a nan to identify the prompt

--- mol_gen_docking/reward/ray_worker.py
+++ mol_gen_docking/reward/ray_worker.py
@@ -1,4 +1,5 @@
 import ray
+import numpy as np
 from typing import List
 
 from mol_gen_docking.reward.grpo_rewards import RewardScorer
@@ -10,8 +11,15 @@ class RewardWorker:
         self._reward_fn = RewardScorer("property", parse_whole_completion=True)
 
     def get_score(self, prompts: List[str], outputs: List[str]) -> List[float]:
-        r = self._reward_fn(prompts, outputs).tolist()
-        return r
+        rewards = self._reward_fn(prompts, outputs).tolist()
+        # Check if there ar Nans or None values in the results
+        for i, (p, c, r) in enumerate(zip(prompts, outputs, rewards)):
+            if r is None or np.isnan(r):
+                print(
+                    f"Warning: Reward is None or NaN for prompt: {p}, completion: {c}"
+                )
+                rewards[i] = 0
+        return rewards
 
 
 @ray.remote(num_cpus=2)
@@ -20,5 +28,5 @@ class RewardWorkerValid:
         self._reward_fn = RewardScorer("valid_smiles", parse_whole_completion=True)
 
     def get_score(self, prompts: List[str], outputs: List[str]) -> List[float]:
-        r = self._reward_fn(prompts, outputs).tolist()
-        return r
+        rewards = self._reward_fn(prompts, outputs).tolist()
+        return rewards
