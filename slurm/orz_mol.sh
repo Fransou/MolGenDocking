5a5be602c1bc80b67d482ff131f806865b03a09d
Philippe Formont <fransou@narval1.narval.calcul.quebec>
2025-06-10 16:24:52 -0400

    refact

--- slurm/orz_mol.sh
+++ slurm/orz_mol.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=orz_mol
 #SBATCH --account=rrg-josedolz
-#SBATCH --time=00:30:00
+#SBATCH --time=02:30:00
 #SBATCH --gres=gpu:4
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=48
@@ -27,6 +27,6 @@ nvidia-smi
 ray status
 
 
-export ORZ_DATA_PATH=$SLURM_TMPDIR/mol_orz
+export ORZ_DATA_PATH=$SLURM_TMPDIR/mol_orz_pocket_desc
 #export DEBUG_MODE=1
 python -m mol_gen_docking.orz_mol
