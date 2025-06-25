2910d01756622c72b9622db35e0cbcfeac9e7b05
Fransou <philippe.form@gmail.com>
2025-06-25 15:57:31 -0400

    [data] Update the flags from <SMILES> to <answer>

--- mol_gen_docking/fast_api_reward_server.py
+++ mol_gen_docking/fast_api_reward_server.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 
     reward_model = RewardScorer(
         path_to_mappings=args.data_path,
-        parse_whole_completion=True,
+        parse_whole_completion=False,
         oracle_kwargs=dict(
             exhaustiveness=args.scorer_exhaustivness,
             ncpu=args.scorer_ncpus,
@@ -56,12 +56,12 @@ if __name__ == "__main__":
     reward_valid_smiles = RewardScorer(
         path_to_mappings=args.data_path,
         reward="valid_smiles",
-        parse_whole_completion=True,
+        parse_whole_completion=False,
     )
     reward_filters = RewardScorer(
         path_to_mappings=args.data_path,
         reward="MolFilters",
-        parse_whole_completion=True,
+        parse_whole_completion=False,
     )
 
     app = FastAPI()
