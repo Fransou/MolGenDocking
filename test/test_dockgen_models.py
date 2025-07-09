8f031569f210d0955d1d3729142bf6b76410e111
Fransou <philippe.form@gmail.com>
2025-07-09 15:57:15 -0400

    [test] Tests first commit (to be changed)

--- test/test_dockgen_models.py
+++ test/test_dockgen_models.py
@@ -37,29 +37,51 @@ mm_messages = get_mm_messages()
 
 @pytest.fixture(scope="module")
 def model_hf():
-    model = AutoModelForCausalLM.from_pretrained(
+    hf_model = AutoModelForCausalLM.from_pretrained(
         HF_MODEL_NAME,
         trust_remote_code=True,
     )
     processor = AutoProcessor.from_pretrained(HF_MODEL_NAME, trust_remote_code=True)
     tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_NAME)
 
-    return model, processor, tokenizer
+    return hf_model, processor, tokenizer
 
 
 @pytest.fixture(scope="module")
-def vllm_model(model_name):
+def vllm_model():
     return vllm.LLM(
-        model_name, task="generate", trust_remote_code=True, gpu_memory_utilization=0.8
+        HF_MODEL_NAME,
+        task="generate",
+        trust_remote_code=True,
+        gpu_memory_utilization=0.8,
     )
 
 
-def test_generation(hf_model, vllm_model, tokenizer_processor):
-    tokenizer, processor = tokenizer_processor
-
+def test_generation(hf_model, vllm_model):
+    hf_model, processor, tokenizer = hf_model
     chat_templated = processor.apply_chat_template(
-        mm_messages, tokenize=True, return_dict=True
+        mm_messages[:2], tokenize=True, return_dict=True
     )
 
-    gen = hf_model.generate(chat_templated)
-    print(gen)
+    hf_generation = hf_model.generate(**chat_templated)
+    # Remove prompt text
+    hf_generation = [
+        g[inp_ids.shape[0] :]
+        for g, inp_ids in zip(hf_generation, chat_templated["input_ids"])
+    ]
+    hf_text = tokenizer.batch_decode(hf_generation, skip_special_tokens=True)
+
+    requests = [
+        vllm.inputs.TokensPrompt(
+            prompt_token_ids=chat_templated["input_ids"][i],
+            multi_modal_data={"image": chat_templated["pixel_values"][i]},
+        )
+        for i in range(chat_templated["input_ids"].shape[0])
+    ]
+    vllm_generations = vllm_model.generate(
+        prompts=requests, sampling_params=vllm.SamplingParams(temperature=0.0)
+    )
+    vllm_text = [g.outputs[0].text for g in vllm_generations]
+
+    for hf_t, vllm_t in zip(hf_text, vllm_text):
+        assert hf_t == vllm_t
