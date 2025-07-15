1f3ccbf37966ebd5244d02c8724e6ab30196ed18
Philippe <philippe.form@gmail.com>
2025-07-15 20:23:09 +0200

    [log] Display validity - uniqueness - diversity

--- mol_gen_docking/fast_api_reward_server.py
+++ mol_gen_docking/fast_api_reward_server.py
@@ -94,34 +94,38 @@ if __name__ == "__main__":
 
         from tdc import Evaluator
         diversity_evaluator = Evaluator(name='Diversity')
-        diversity_scores = {
+        diversity_scores_dict = {
             p: diversity_evaluator(group_prompt_smiles[p]) for p in unique_prompts
         }
+        diversity_score = [float(diversity_scores_dict[p]) for p in prompts]
+        print(diversity_score)
 
         validity_evaluator = Evaluator(name='Validity')
-        validity_scores = {
+        validity_scores_dict = {
             p: validity_evaluator(group_prompt_smiles[p]) for p in unique_prompts
         }
+        validity_score = [float(validity_scores_dict[p]) for p in prompts]
 
         uniqueness_evaluator = Evaluator(name='Uniqueness')
-        uniqueness_scores = {
+        uniqueness_scores_dict = {
             p: uniqueness_evaluator(group_prompt_smiles[p]) for p in unique_prompts
         }
-        print(validity_scores)
+        uniqueness_score = [float(uniqueness_scores_dict[p]) for p in prompts]
 
 
         # rewards = ray.get(rewards_job)
         rewards = valid_reward
 
+        print(validity_score, uniqueness_score, diversity_score)
         result = {
             "rewards": rewards,
             "scores": rewards,
             "extra_logs": {
                 "property_scores": rewards,
                 "valid_smiles_scores": valid_reward,
-                "validity": [float(validity_scores[p]) for p in unique_prompts],
-                "uniqueness": [float(uniqueness_scores[p]) for p in unique_prompts],
-                "diversity": [float(diversity_scores[p]) for p in unique_prompts],
+                "validity": validity_score,
+                "uniqueness": uniqueness_score,
+                "diversity": diversity_score,
                 # "mol_filters": filter_reward,
             },
         }
