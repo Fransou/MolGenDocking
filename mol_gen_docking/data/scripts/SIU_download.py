8b983b8c5c654bdb7b82ca2969827c4693cd5137
Fransou <philippe.form@gmail.com>
2025-10-27 16:29:05 -0400

    Fix server

---
+++ mol_gen_docking/data/scripts/SIU_download.py
@@ -0,0 +1,72 @@
+import argparse
+import json
+import logging
+import os
+import pickle
+import shutil
+
+import numpy as np
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+# Set up logging to INFO level
+logging.basicConfig(level=logging.INFO)
+
+
+def get_pockets_info(data_path: str) -> dict:
+    with open(os.path.join(data_path, "final_dic.pkl"), "rb") as f:
+        siu_data = pickle.load(f)
+    pockets_info = {}
+    assert os.path.exists(os.path.join(data_path, "result_structures"))
+    os.makedirs(os.path.join(data_path, "pdb_files"), exist_ok=True)
+    for uni_prot_id in tqdm(siu_data.keys()):
+        for ligand_pocket in siu_data[uni_prot_id]:
+            dir = ligand_pocket["source_data"][1]
+            pocket_coordinates = np.concatenate(ligand_pocket["pocket_coordinates"])
+            break
+        file = os.path.join(
+            data_path, "result_structures", uni_prot_id, dir + "_rmW.pdb"
+        )
+        shutil.move(file, os.path.join(data_path, "pdb_files", file))
+
+        average_pIC50 = np.mean(
+            [ligand_pocket["label"]["ic50"] for ligand_pocket in siu_data[uni_prot_id]]
+        )
+        pocket_size = (
+            pocket_coordinates.max(axis=0) - pocket_coordinates.min(axis=0)
+        ).tolist()
+        pocket_center = (
+            (pocket_coordinates.max(axis=0) + pocket_coordinates.min(axis=0)) / 2
+        ).tolist()
+
+        pockets_info[file.replace(".pdb", "")] = {
+            "size": pocket_size,
+            "center": pocket_center,
+            "metadata": {
+                "size": pocket_size,
+                "center": pocket_center,
+                "sequence": "",
+                "avg_pIC50": average_pIC50,
+                "prot_id": uni_prot_id,
+                "origin": "SIU",
+            },
+        }
+    return pockets_info
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate the RL dataset for molecular generation with docking instructions"
+    )
+
+    parser.add_argument(
+        "--data-path", type=str, default="data/SIU", help="Path to the dataset"
+    )
+    args = parser.parse_args()
+
+    os.makedirs(args.data_path, exist_ok=True)
+
+    pockets_info = get_pockets_info(data_path=args.data_path)
+
+    with open(os.path.join(args.data_path, "pockets_info.json"), "w") as f:
+        json.dump(pockets_info, f, indent=4)
