5d85d3b1269a5406631adb56b543e74753a944cc
Philippe Formont <66243927+Fransou@users.noreply.github.com>
2025-10-30 18:15:26 -0400

    Data/reactions uspto (#42)
    * USPTO
    
    * Adapt tests and rl reward
    
    * fix lists in metadata

---
+++ test/test_rewards/test_rl_rewards_reac.py
@@ -0,0 +1,39 @@
+import ray
+
+from mol_gen_docking.reward.rl_rewards import RewardScorer
+
+from .utils import (
+    DATA_PATH,
+)
+
+try:
+    ray.init(num_cpus=16)
+except Exception:
+    ray.init()
+
+
+property_scorer = RewardScorer(DATA_PATH, "property", rescale=False)
+
+
+def test_reaction():
+    target = "Nc1ccccc1C=O + O=C1CC1N=C"
+    completions = [
+        "<answer> {} </answer>".format(v)
+        for v in [
+            target,
+            "c1(C=O)c(N)cccc1, C=NC1CC1=O",
+            "c1(C=O)c(N)cccc1 + C=NC1CC1=O",
+            "Nc1ccccc1C=O, CCC",
+            "ksdjgf",
+        ]
+    ] * 4
+
+    metadata = [
+        {"objectives": [obj], "properties": [""], "target": [target]}
+        for obj in ["product", "product_full", "reactant", "reactant_full"]
+        for _ in range(5)
+    ]
+    rewards = property_scorer(
+        [""] * len(completions), completions, metadata, use_pbar=False
+    )
+    assert rewards == [1.0, 1.0, 1.0, 0.1, 0] * 4
