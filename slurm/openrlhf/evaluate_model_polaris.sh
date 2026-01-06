cd68e74b5cfc476ee56f1023fe4f23d176379ab8
Philippe Formont <fransou@l2.nibi.sharcnet>
2026-01-06 14:39:46 -0500

    fix sbatch file

--- slurm/openrlhf/evaluate_model_polaris.sh
+++ slurm/openrlhf/evaluate_model_polaris.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=batch_inference_molgen
 #SBATCH --account=def-ibenayed
-#SBATCH --time=06:00:00
+#SBATCH --time=03:00:00
 #SBATCH --gpus=h100:4
 #SBATCH --mem=248G
 #SBATCH --cpus-per-task=32
@@ -48,7 +48,7 @@ ray job submit \
    --zero_stage 3 \
    --bf16 \
    --best_of_n 3 \
-   --rollout_batch_size 5000 \
+   --rollout_batch_size 10000 \
    --iter $SLURM_ARRAY_TASK_ID \
    --apply_chat_template \
    --dataset $DATA_PATH/eval_concatenated.jsonl \
