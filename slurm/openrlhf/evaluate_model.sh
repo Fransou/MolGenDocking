6804f27a6a642020082b72b4ec13b6d8cf25f5ab
Philippe Formont <66243927+Fransou@users.noreply.github.com>
2026-02-10 12:36:20 -0500

    Slurm/yaml (#104)
    * slurm parametrizing
    
    * Notebook to generate prompts
    
    * slurm parametrizing
    
    * fix path
    
    ---------
    
    Co-authored-by: Philippe Formont <fransou@rorqual4.rorqual.calcul.quebec>

--- slurm/openrlhf/evaluate_model.sh
+++ slurm/openrlhf/evaluate_model.sh
@@ -1,67 +1,54 @@
 #!/bin/bash
 #SBATCH --job-name=batch_inference_molgen
 #SBATCH --account=def-ibenayed
-#SBATCH --time=06:00:00
+#SBATCH --time=00:00:00
 #SBATCH --gpus=h100:4
 #SBATCH --mem=248G
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=12
 #SBATCH --tasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
-#SBATCH --array=0-2
+#SBATCH --array=0-0
 
-export WORKING_DIR=$HOME/MolGenDocking
+DATASET=$1
+CONFIG=$2
+
+DATA_PATH=$SLURM_TMPDIR/$DATASET
+WORKING_DIR=$HOME/MolGenDocking
+export DASHBOARD_PORT=$((8000 + SLURM_ARRAY_TASK_ID))
 
 source $HOME/.bashrc
 source $HOME/OpenRLHF/bin/activate
 
-export DATASET=molgendata
 cp $SCRATCH/MolGenData/$DATASET.tar.gz $SLURM_TMPDIR
 cd $SLURM_TMPDIR
 tar -xzf $DATASET.tar.gz
 cd $WORKING_DIR
+
 cp data/properties.csv $SLURM_TMPDIR
-export DATA_PATH=$SLURM_TMPDIR/$DATASET
+ray start --head --node-ip-address 0.0.0.0 --dashboard-port=$DASHBOARD_PORT
+# ssh -N -f -R ${DASHBOARD_PORT}:localhost:${DASHBOARD_PORT} $SLURM_JOB_USER@rorqual4
 
-export PRETRAIN=$1
 
-ray start --head --node-ip-address 0.0.0.0
+#export DEBUG_MODE=1
+ray job submit \
+   --address="http://127.0.0.1:$DASHBOARD_PORT" \
+   --runtime-env-json='{"setup_commands": ["wandb offline"]}' \
+   -- python3 -m openrlhf.cli.batch_inference \
+   --config $CONFIG \
+   --iter $SLURM_ARRAY_TASK_ID
+
 
 export docking_oracle=autodock_gpu
 export scorer_exhaustiveness=4
-
-if [[ -n "$5" ]]; then
-    TP_SIZE=$5
+if [ "$DATASET" == "molgendata" ]; then
+    python -m mol_gen_docking.score_completions \
+      --input_file $CONFIG \
+      --batch_size 1024 \
+      --mol-generation
 else
-    TP_SIZE=4
+    python -m mol_gen_docking.score_completions \
+      --input_file $CONFIG \
+      --batch_size 1024
 fi
-echo "TP size $TP_SIZE"
-#export DEBUG_MODE=1
-ray job submit \
-   --address="http://127.0.0.1:8265" \
-   --runtime-env-json='{"setup_commands": ["wandb offline"]}' \
-   -- python3 -m openrlhf.cli.batch_inference \
-   --pretrain $PRETRAIN \
-   --max_len 4096 \
-   --max_new_tokens 32768 \
-   --zero_stage 3 \
-   --bf16 \
-   --best_of_n 128 \
-   --rollout_batch_size 100 \
-   --iter $SLURM_ARRAY_TASK_ID \
-   --apply_chat_template \
-   --dataset $DATA_PATH/$3 \
-   --input_key messages \
-   --label_key meta \
-   --eval_task generate_vllm \
-   --output_path $2_$SLURM_ARRAY_TASK_ID \
-   --top_p 0.95 \
-   --temperature $4 \
-   --system_prompt system_prompts/vanilla.json \
-   --tp_size $TP_SIZE
-
-python -m mol_gen_docking.score_completions \
-    --input_file $2_$SLURM_ARRAY_TASK_ID.jsonl \
-    --batch_size 1024 \
-    --mol-generation
